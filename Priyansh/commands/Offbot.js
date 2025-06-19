e516074cb31b1b5b2ca3582d96220160971a8f20
XD0143 <Talhakhansk55@gmail.com>
2025-06-20 02:26:44 +0500

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -8,7 +8,7 @@ module.exports.config = {
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = ["100080347467595"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Chuza", event.threadID, event.messageID);
-  api.sendMessage(`[ OK Chuza  janu  ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
+    const permission = ["100065216344877"];
+  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] 𝐘𝐨𝐮 𝐝𝐨𝐧'𝐭 𝐡𝐚𝐯𝐞 𝐩𝐞𝐫𝐦𝐢𝐬𝐬𝐢𝐨𝐧 𝐭𝐨 𝐮𝐬𝐞 𝐭𝐡𝐢𝐬 𝐜𝐨𝐦𝐦𝐚𝐧𝐝, 𝐓𝐡𝐢𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐎𝐰𝐧𝐞𝐫 𝐓𝐚𝐥𝐡𝐚", event.threadID, event.messageID);
+  api.sendMessage(`[ 𝐎𝐤𝐢𝐢𝐰 𝐁𝐨𝐬𝐬 🙊  ] ${global.config.BOTNAME} 𝐁𝐨𝐭 𝐚𝐫𝐞 𝐧𝐨𝐰 𝐭𝐮𝐫𝐧𝐞𝐝 𝐎𝐟𝐟 (𝐎𝐤𝐢𝐢𝐰 𝐛𝐲𝐞 𝐆𝐡𝐚𝐫𝐞𝐞𝐛𝐎𝐧 🤧😂).`,event.threadID, () =>process.exit(0))
 }
