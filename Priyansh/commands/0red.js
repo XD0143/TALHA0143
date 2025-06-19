deb61ebf32e46f772e7b043ff73895a9121348bb
XD0143 <Talhakhansk55@gmail.com>
2025-06-20 02:32:52 +0500

    Update 0red.js

--- Priyansh/commands/0red.js
+++ Priyansh/commands/0red.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "red",
 	version: "11.9.7",
-	hasPermssion: 0,
+	hasPermssion: 2,
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "redroom vids",
 	commandCategory: "Not For Kids",
